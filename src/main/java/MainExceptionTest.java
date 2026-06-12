5d34f60aea5f48a56de16bddda4689da41ba10ef
kseni <kseniya.orlova.82@gmail.com>
2026-06-12 13:10:16 +0200

    Exception1

--- src/main/java/MainExceptionTest.java
+++ src/main/java/MainExceptionTest.java
@@ -1,14 +1,65 @@
+import java.util.ArrayList;
+
 public class MainExceptionTest {
     public static void main(String[] args) {
-        int a = 6;
-        int b = 0;
-        try{
-        int c = a/b;
-        System.out.println("c = " +c);
+        int a = 6, b = 0, index = 0, index1 = 3;
+        String s = null, s1 = "hello", s2 = "23a";
+        int[]ar = {1,2,3};
+        ArrayList<Integer> list  = new ArrayList<>();
+        list.add(3);
+        list.add(2);
+
+        try {
+            int c = a / b;
+            //ArithmeticException
+            System.out.println("c = " + c);
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();//
+        }
+        try {
+
+            char sym = s.charAt(1);
+            //NullPointerException
+            System.out.println(sym);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
+        try {
+            char sym = s1.charAt(-1);
+            //StringIndexOutOfBoundsException
+            System.out.println(sym);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        try {
+            int res = ar[4];
+//ArrayIndexOutOfBoundsException
+            System.out.println("result = " + res);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            int res = Integer.parseInt(s2);
+//NumberFormatException
+            System.out.println("res = " + res);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        try {
+            int res = list.get(index1);
+//IndexOutOfBoundsException
+            System.out.println("res  = " + res);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
         System.out.println("The end");
     }
+
+
 }
